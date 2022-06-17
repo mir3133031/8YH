231d59c13470241d73480682a486e03853a1256e
мир <106350647+mir3133031@users.noreply.github.com>
2022-06-17 14:40:36 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+let question = prompt(`number`)
+
+if (question >= 100) {
+  alert(`good`)
+} else {
+
+  let i = 0;
+  do {
+    
+     prompt(`number`);
+    i++;
+  } while (i < 6  );
+}
+
+// let i = 0;
+  // while (i < 3) {
+  //   prompt(`number`);
+  //   i++;
+  // }
